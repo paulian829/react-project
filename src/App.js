8425a0e43453444a76b33ce023d391320dd5896c
paulian829 <60558503+paulian829@users.noreply.github.com>
2025-08-20 14:32:53 +0800

    Add Hello Work and 404 pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HelloWork from './HelloWork';
+import NotFound from './NotFound';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const path = window.location.pathname;
+  return path === '/' ? <HelloWork /> : <NotFound />;
 }
 
 export default App;
